61d9f03f15a2e23a83ad0cc143bf463cf535ca86
moabdali <64987634+moabdali@users.noreply.github.com>
2020-06-19 18:34:24 -0500

    calculates surrounding squares

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # pySweeper
  minesweeper clone for practice
 
+
+
+v0.2:
+	calculates the number of bombs correctly according to a few tests.
+	immediate todo for next version: allow user to click for bombs, and reveal the number of surrounding bombs.
+	automatic revealing of 'zero squares' will be done in a future build.
 v0.1: 
 	places mines and shows the playing field
 	immediate todo for the next version: calculate and show the number of bombs surrounding any spot that isn't a bomb 
+	
--- pySweeper.py
+++ pySweeper.py
@@ -41,11 +41,13 @@ while bombCount > 0:
     if playingField[bombRow][bombColumn][0] == 1:
         print("already bombed")
         continue
+
     elif playingField[bombRow][bombColumn][0] == 0:
         print("laying mine")
         bombCount -= 1
         playingField[bombRow][bombColumn][0] = 1
         continue
+    
     else:
         print("dire error!")
         quit
@@ -58,4 +60,99 @@ for rrows in playingField:
             print(f" # ",end="")
         else:
             print(f" ? ",end="")
-    print("")        
+    print("")
+
+# square = 0 = bomb (0 = safe, 1 = bomb), 1= top left, 2= top middle, 3 = top right, 4 = middle left,
+#          5 = middle right, 6 = bottom left, 7 = bottom middle, 8 = bottom right,
+#          9 = status (0 = unrevealed, 1 = revealed), 10 = number of bombs surrounding spot
+
+
+for r in range(rows):
+    
+    for c in range(columns):
+        surrBombs = 0
+        if playingField[r][c][0] == 1:
+            print("You're standing on a bomb.")
+            continue
+        print(f"searching for bombs around ({r},{c})")
+        #try top left
+        try:
+            if playingField[r-1][c-1][0] == 1:
+                print("I found a bomb top left")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+            
+        #try top middle:
+        try:
+            if playingField[r-1][c][0] == 1:
+                print("I found a bomb above")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+            
+        #try top right
+        try:
+            if playingField[r-1][c+1][0] == 1:
+                print("I found a bomb top right")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+
+        #try left
+        try:
+            if playingField[r][c-1][0] == 1:
+                print("I found a bomb left")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+
+        #try right
+        try:
+            if playingField[r][c+1][0] == 1:
+                print("I found a bomb right")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+
+
+
+        #try bottom left
+        try:
+            if playingField[r+1][c-1][0] == 1:
+                print("I found a bomb bottom left")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+
+        #try bottom
+        try:
+            if playingField[r+1][c][0] == 1:
+                print("I found a bomb bottom")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+
+        #try bottom right
+        try:
+            if playingField[r+1][c+1][0] == 1:
+                print("I found a bomb bottom right")
+                surrBombs += 1
+        except:
+            surrBombs = surrBombs
+            
+        print(f"I found {surrBombs} bombs")
+        playingField[r][c][10]=surrBombs
+
+
+
+
+
+for r in range(rows):
+   
+    for c in range(columns):
+        if playingField[r][c][0] == 1:
+            print( " # ",end="")
+            continue
+        print(f" {playingField[r][c][10]} ",end="")
+    print("")
